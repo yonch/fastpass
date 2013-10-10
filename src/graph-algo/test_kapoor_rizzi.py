6ad82ed83c6ca9817691eedd80b90acdc5830188
Jonathan Perry <yonch@yonch.com>
2013-10-10 15:07:50 -0400

    Changed Kapoor-Rizzi test to a Python UnitTest

--- src/graph-algo/test_kapoor_rizzi.py
+++ src/graph-algo/test_kapoor_rizzi.py
@@ -1,23 +1,30 @@
 '''
-Created on Oct 9, 2013
+Created on Oct 10, 2013
 
 @author: yonch
 '''
+import unittest
 
 from kapoor_rizzi import *
 from graph_util import graph_util
 
-if __name__ == '__main__':
-    KR = kapoor_rizzi()
-    
-    degree = 33
-    partition_n_nodes = 5
-    
-    # generate random graph
-    g = graph_util().generate_random_regular_bipartite(partition_n_nodes, degree)
-    
-    # generate arbitrary partitions for approximation algo
-    arbitrary = [graph_util().generate_random_regular_bipartite(partition_n_nodes, 1) for i in xrange((degree % 2) + 1)]
-    
-    KR.solve(degree, g, arbitrary)
-    
+class Test(unittest.TestCase):
+    def test_approx_coloring(self):
+        
+        KR = kapoor_rizzi()
+        
+        degree = 33
+        partition_n_nodes = 5
+        
+        # generate random graph
+        g = graph_util().generate_random_regular_bipartite(partition_n_nodes, degree)
+        
+        # generate arbitrary partitions for approximation algo
+        arbitrary = [graph_util().generate_random_regular_bipartite(partition_n_nodes, 1) for i in xrange((degree % 2) + 1)]
+        
+        solution = KR.solve(degree, g, arbitrary)
+
+
+if __name__ == "__main__":
+    #import sys;sys.argv = ['', 'Test.testName']
+    unittest.main()

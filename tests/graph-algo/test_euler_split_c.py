6b42d08db1f381a7a36694020cb19e51017b0d53
Amy Ousterhout <aousterh@mit.edu>
2013-10-25 12:02:24 -0400

    Added test of Euler split C code with irregular graphs.

--- tests/graph-algo/test_euler_split_c.py
+++ tests/graph-algo/test_euler_split_c.py
@@ -21,8 +21,6 @@ class Test(unittest.TestCase):
         generator = graph_util()
         for deg in xrange(2, 11, 2):
             for n_side in xrange(2*deg+4,33,7):
-                print("testing with n=%d, degree=%d" % (n_side, deg))
-
                 g_p = generator.generate_random_regular_bipartite(n_side, deg)
                 
                 # Create the graph in C
@@ -65,7 +63,46 @@ class Test(unittest.TestCase):
 
         pass
 
-        # TODO: write more tests, especially for non-regular graphs.
+
+    def test_irregular_graphs(self):
+        generator = graph_util()
+        for max_deg in xrange(2, 11, 2):
+            for n_side in xrange(2 * max_deg + 4,33,7):
+                for e in xrange(4, n_side * max_deg - 4, 10):
+                    g_p = generator.generate_random_even_degree_bipartite(n_side, max_deg, e)
+
+                    # Create the graph in C
+                    g_c = graph.create_graph_test(n_side);
+                    g_c_copy = graph.create_graph_test(n_side)
+                    for edge in g_p.edges_iter():
+                        graph.add_edge(g_c, edge[0], edge[1])
+                        graph.add_edge(g_c_copy, edge[0], edge[1])
+     
+                    # Create the output graphs
+                    g1_c = graph.create_graph_test(n_side)
+                    g2_c = graph.create_graph_test(n_side)
+
+                    eulersplit.split(g_c_copy, g1_c, g2_c)
+
+                    # Check that the input graph is now empty
+                    self.assertEqual(graph.get_max_degree(g_c_copy), 0)
+                    for node in xrange(2 * n_side):
+                        self.assertEqual(graph.get_degree(g_c_copy, node), 0)
+
+                    # Check that graphs have the correct max_degree
+                    self.assertEqual(graph.get_max_degree(g1_c), graph.get_max_degree(g_c) / 2)
+                    self.assertEqual(graph.get_max_degree(g2_c), graph.get_max_degree(g_c) / 2)
+
+                    # Check that the combination of the two graphs equals the original graph
+                    graph.add_graph(g1_c, g2_c)
+                    self.assertTrue(graph.are_equal(g_c, g1_c))
+                   
+                    graph.destroy_graph_test(g_c)
+                    graph.destroy_graph_test(g_c_copy)
+                    graph.destroy_graph_test(g1_c)
+                    graph.destroy_graph_test(g2_c)
+
+        pass
 
 if __name__ == "__main__":
     unittest.main()

480fd69074d7fd5c77af6a8f060dc012c6281650
Jonathan Perry <yonch@yonch.com>
2014-01-06 19:41:39 -0800

    Really verbose debugging for checksum

--- src/protocol/fpproto.c
+++ src/protocol/fpproto.c
@@ -47,7 +47,13 @@ static __sum16 fastpass_checksum(u8 *pkt, u32 len, __be32 saddr, __be32 daddr,
 	u32 seq_hash = jhash_3words((u32)seqno, seqno >> 32, (u32)ack_seq,
 			ack_seq >> 32);
 	__wsum csum = csum_partial(pkt, len, seq_hash);
-	fp_debug("seq_hash %u csum %u len %u\n", seq_hash, csum, len);
+	fp_debug("seq_hash 0x%X csum 0x%X len %u\n", seq_hash, csum, len);
+	fp_debug("csum_partial 1/6 0x%X 2/6 0x%X 3/6 0x%X 4/6 0x%X 5/6 0x%X \n",
+			csum_partial(pkt, len * 1 / 6, seq_hash),
+			csum_partial(pkt, len * 2 / 6, seq_hash),
+			csum_partial(pkt, len * 3 / 6, seq_hash),
+			csum_partial(pkt, len * 4 / 6, seq_hash),
+			csum_partial(pkt, len * 5 / 6, seq_hash));
 	return csum_tcpudp_magic(0, 0, len, IPPROTO_FASTPASS, csum);
 	/* TODO: reinstate checksumming saddr and daddr */
 	return csum_tcpudp_magic(saddr, daddr, len, IPPROTO_FASTPASS, csum);

c493578239966fe5c59ad55686f89c7191dbdf9b
Jonathan Perry <yonch@yonch.com>
2014-01-10 02:39:45 -0500

    Removed too-verbose debug prints for checksum (now that the bugs there seem to be solved)

--- src/protocol/fpproto.c
+++ src/protocol/fpproto.c
@@ -47,6 +47,7 @@ static __sum16 fastpass_checksum(u8 *pkt, u32 len, __be32 saddr, __be32 daddr,
 	u32 seq_hash = jhash_3words((u32)seqno, seqno >> 32, (u32)ack_seq,
 			ack_seq >> 32);
 	__wsum csum = csum_partial(pkt, len, seq_hash);
+#if 0
 	fp_debug("ptr 0x%p seq_hash 0x%X csum 0x%X len %u csum_partial 0 0x%X 1 0x%X 2 0x%X 3 0x%X 4 0x%X 5 0x%X 6 0x%X\n",
 			pkt, seq_hash, csum, len,
 			csum_partial(pkt, 0, seq_hash),
@@ -56,6 +57,7 @@ static __sum16 fastpass_checksum(u8 *pkt, u32 len, __be32 saddr, __be32 daddr,
 			csum_partial(pkt, 4, seq_hash),
 			csum_partial(pkt, 5, seq_hash),
 			csum_partial(pkt, 6, seq_hash));
+#endif
 	return csum_tcpudp_magic(0, 0, len, IPPROTO_FASTPASS, csum);
 	/* TODO: reinstate checksumming saddr and daddr */
 	return csum_tcpudp_magic(saddr, daddr, len, IPPROTO_FASTPASS, csum);

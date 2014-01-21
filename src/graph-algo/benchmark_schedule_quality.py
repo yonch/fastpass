9ebaaec7b2bc71d37233431904caa6f6ff723c45
Amy Ousterhout <aousterh@mit.edu>
2014-01-21 13:37:48 -0500

    Added shortest job first to schedule quality benchmark

--- src/graph-algo/benchmark_schedule_quality.py
+++ src/graph-algo/benchmark_schedule_quality.py
@@ -26,6 +26,7 @@ class admissible_runner(object):
     '''Runs an admissible traffic experiment.'''
 
     ROUND_ROBIN = 0
+    SHORTEST_JOB_FIRST = 1
 
     def __init__(self, num_nodes, duration, warm_up_duration, request_size):
         self.num_nodes = num_nodes
@@ -118,6 +119,75 @@ class admissible_runner(object):
 
         return observed_util
 
+    def run_shortest_job_first_admissible(self):
+        # initialization
+        q_bin = fpring.fp_ring_create(structuressjf.NUM_BINS_SHIFT)
+        q_urgent = fpring.fp_ring_create(2 * structuressjf.NODES_SHIFT + 1)
+        q_head = fpring.fp_ring_create(2 * structuressjf.NODES_SHIFT)
+        q_admitted_out= fpring.fp_ring_create(structuressjf.BATCH_SHIFT)
+
+        core = structuressjf.create_admission_core_state()
+        structuressjf.alloc_core_init(core, q_bin, q_bin, q_urgent, q_urgent)
+        status = structuressjf.create_admissible_status(False, 0, 0, self.num_nodes,
+                                                     q_head, q_admitted_out)
+        admitted_batch = structuressjf.create_admitted_batch()
+
+        for i in range(0, structuressjf.NUM_BINS):
+            empty_bin = structuressjf.create_bin(structuressjf.LARGE_BIN_SIZE)
+            fpring.fp_ring_enqueue(q_bin, empty_bin)
+
+        admissiblesjf.enqueue_head_token(q_urgent)
+
+        num_admitted = 0
+        num_requested = 0
+        # TODO: can we run this so that request arrivals are inter-leaved with
+        # getting admissible traffic? would be more realistic.
+        current_request = 0
+        req = self.requests[current_request]
+        for t in range(self.duration):
+            # Issue new requests
+            while int(req.time) == t:
+                num_requested += self.request_size
+                admissiblesjf.add_backlog(status, req.src, req.dst, self.request_size)
+                self.pending_requests[(req.src, req.dst)].append(pending_request(self.request_size, t))
+                current_request += 1
+   
+                req = self.requests[current_request]
+                
+            if t % structuressjf.BATCH_SIZE != structuressjf.BATCH_SIZE - 1:
+                continue
+
+            # Get admissible traffic for this batch
+            admissiblesjf.get_admissible_traffic(core, status, admitted_batch, 0, 1, 0)
+            
+            if t < self.warm_up_duration:
+                continue
+
+            # Record stats
+            for i in range(structuressjf.BATCH_SIZE):
+                admitted_i = admissiblesjf.dequeue_admitted_traffic(status)
+                num_admitted += admitted_i.size
+
+                for e in range(admitted_i.size):
+                    edge = structuressjf.get_admitted_edge(admitted_i, e)
+                    req_list = self.pending_requests[(edge.src, edge.dst)]
+                    if len(req_list) < 1:
+                        raise AssertionError
+                    req_list[0].size -= 1
+                    if req_list[0].size == 0:
+                        # record flow completion time
+                        last_t_slot = t + i
+                        fct = last_t_slot - req_list[0].request_time
+                        self.flow_completion_times.append(fct)
+                        del req_list[0]
+                
+        capacity = (self.duration - self.warm_up_duration) * self.num_nodes
+        observed_util = float(num_admitted) / capacity
+                                                               
+        # should clean up memory
+
+        return observed_util
+
     def run_admissible(self, admissible_type):
         '''Run an admissible algorithm of a specific type. 0=round-robin, 1=sjf, 2=random.'''
 
@@ -132,6 +202,8 @@ class admissible_runner(object):
 
         if admissible_type == self.ROUND_ROBIN:
             return self.run_round_robin_admissible()
+        elif admissible_type == self.SHORTEST_JOB_FIRST:
+            return self.run_shortest_job_first_admissible()
 
 class Test(unittest.TestCase):
 
@@ -139,7 +211,8 @@ class Test(unittest.TestCase):
         """Benchmarks the flow completion time of different admissible traffic algorithms."""
 
         fractions = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 0.95, 0.99]
-        algorithms = [(admissible_runner.ROUND_ROBIN, "round_robin")]
+        algorithms = [(admissible_runner.ROUND_ROBIN, "round_robin"),
+                      (admissible_runner.SHORTEST_JOB_FIRST, "shortest_job_first")]
         num_nodes = 256
         # keep both durations an even number of batches so that bin pointers return to queue_0
         warm_up_duration = ((10000 + 127) / 128) * 128
--- src/graph-algo/graph_fct_cdf.R
+++ src/graph-algo/graph_fct_cdf.R
@@ -16,8 +16,11 @@ library(scales)
 
 data <- read.csv("output.csv", sep=",")
 
-ggplot(data, aes(fct, group=interaction(algo, as.factor(observed_util)), colour=interaction(algo, as.factor(observed_util)))) + stat_ecdf() +
+ggplot(data, aes(fct, linetype=as.factor(target_util), color=algo)) + stat_ecdf() +
              labs(x = "Flow Completion Time (timeslots)", y = "Fraction of Samples",
                   title = "CDF of Flow Completion Times") +
              scale_x_log10(breaks= trans_breaks("log10", function(x) 10^x)) + 
-             annotation_logticks() + scale_colour_discrete(name="Algorithm/Utilization")
+             annotation_logticks() + scale_colour_discrete(name="Algorithm") +
+             scale_linetype_discrete(name="Target Utilization")
+             
+

b8e3733180af446562f96fed285cf47da4924f30
Amy Ousterhout <aousterh@mit.edu>
2014-02-17 15:02:25 -0500

    added error bars to admissible traffic latency benchmark plot

--- src/graph-algo/graph_admissible_vs_utilization.R
+++ src/graph-algo/graph_admissible_vs_utilization.R
@@ -16,6 +16,57 @@
 # type 0 (default) is for round robin
 # type 1 is for shortest remaining job first
 
+## Based on: http://www.cookbook-r.com/Manipulating_data/Summarizing_data/
+## Summarizes data.
+## Gives count, mean, standard deviation, standard error of the mean, and confidence interval (default 95%).
+##   data: a data frame.
+##   measurevar: the name of a column that contains the variable to be summariezed
+##   groupvars: a vector containing names of columns that contain grouping variables
+##   na.rm: a boolean that indicates whether to ignore NA's
+##   conf.interval: the percent range of the confidence interval (default is 95%)
+summarySE <- function(data=NULL, measurevar, groupvars=NULL, na.rm=FALSE,
+                      conf.interval=.95, .drop=TRUE) {
+    require(plyr)
+
+    # New version of length which can handle NA's: if na.rm==T, don't count them
+    length2 <- function (x, na.rm=FALSE) {
+        if (na.rm) sum(!is.na(x))
+        else       length(x)
+    }
+
+    # This does the summary. For each group's data frame, return a vector with
+    # N, mean, and sd
+    datac <- ddply(data, groupvars, .drop=.drop,
+      .fun = function(xx, col) {
+        c(N    = length2(xx[[col]], na.rm=na.rm),
+          mean = mean   (xx[[col]], na.rm=na.rm),
+          sd   = sd     (xx[[col]], na.rm=na.rm),
+          p_95 = quantile(xx[[col]], 0.95, na.rm=na.rm),
+          p_5 = quantile(xx[[col]], 0.05, na.rm=na.rm)
+        )
+      },
+      measurevar
+    )
+
+    # Rename the "mean" column    
+    datac <- rename(datac, c("mean" = measurevar))
+    
+    # Rename the percentile columns
+    datac <- rename(datac, c("p_5.5%" = "p_5"))
+    datac <- rename(datac, c("p_95.95%" = "p_95"))
+
+    datac$se <- datac$sd / sqrt(datac$N)  # Calculate standard error of the mean
+
+    # Confidence interval multiplier for standard error
+    # Calculate t-statistic for confidence interval: 
+    # e.g., if conf.interval is .95, use .975 (above/below), and use df=N-1
+    ciMult <- qt(conf.interval/2 + .5, datac$N-1)
+    datac$ci <- datac$se * ciMult
+
+    return(datac)
+}
+
+
 # use the ggplot2 library
 library(ggplot2)
 
@@ -40,11 +91,14 @@ theme_set(theme_bw(base_size=12))
 
 new_data = data[data$nodes > 128,]
 
-ggplot(new_data, aes(x=observed_utilization, y=time,
+summary <- summarySE(new_data, measurevar="time", groupvars=c("observed_utilization", "nodes"))
+
+ggplot(summary, aes(x=observed_utilization, y=time,
              color=as.factor(nodes), shape=as.factor(nodes))) +
              geom_point() + geom_line() +
+             geom_errorbar(aes(ymin=p_5, ymax=p_95), width=0.01) +
              scale_color_discrete(name="Nodes", guide = guide_legend(reverse = TRUE)) +
-             scale_shape_discrete(name="Nodes", guide = guide_legend(reverse = TRUE)) +
+             scale_shape_manual(name="Nodes", guide = guide_legend(reverse = TRUE), values=c(15, 16, 17, 18)) +
              labs(x = "Network Utilization (%)",
                   y = "Latency (microseconds)")
 

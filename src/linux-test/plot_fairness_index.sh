fbb526bf0a90bdef9cc4155905f85ae27900b7d2
Amy Ousterhout <aousterh@mit.edu>
2014-02-18 17:37:38 -0500

    plotted fairness index of fastpass vs. baseline

---
+++ src/linux-test/plot_fairness_index.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+BIN_SIZE=100000000 # 100ms
+
+# compute fairness for fastpass
+python compute_fairness.py $BIN_SIZE fairness_5_senders_start_incrementally_fastpass_port_1100.csv fairness_5_senders_start_incrementally_fastpass_port_1101.csv fairness_5_senders_start_incrementally_fastpass_port_1102.csv fairness_5_senders_start_incrementally_fastpass_port_1103.csv fairness_5_senders_start_incrementally_fastpass_port_1104.csv > output_fastpass.csv
+
+# compute fairness for baseline
+python compute_fairness.py $BIN_SIZE fairness_5_senders_start_incrementally_baseline_port_1100.csv fairness_5_senders_start_incrementally_baseline_port_1101.csv fairness_5_senders_start_incrementally_baseline_port_1102.csv fairness_5_senders_start_incrementally_baseline_port_1103.csv fairness_5_senders_start_incrementally_baseline_port_1104.csv > output_baseline.csv
+
+# graph
+R < ./graph_fairness_index_over_time.R --save

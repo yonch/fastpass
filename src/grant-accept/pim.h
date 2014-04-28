00a2cb74aaa53113bf636580c4e57f3d9f7db60e
Jonathan Perry <yonch@yonch.com>
2014-04-28 15:16:31 -0400

    API changes to PIM, added a big structure with all the data needed for PIM

--- src/grant-accept/pim.h
+++ src/grant-accept/pim.h
@@ -9,32 +9,34 @@
 #define PIM_H_
 
 #include "grant-accept.h"
-#include "partitioned-edgelist.h"
+#include "edgelist.h"
 
 /**
  * A structure for the state of a grant partition
  */
-struct pim_grant_partn_state {
-
+struct pim_state {
+	struct ga_adj requests[GA_N_PARTITIONS];
+	struct ga_partd_edgelist grants;
+	struct ga_adj grants_adj[GA_N_PARTITIONS];
+	struct ga_partd_edgelist accepts;
+	struct
 };
 
 /**
  * For all destination (right-hand) nodes in partition 'partition_index',
  *    selects edges to grant. These are added to 'grants'.
  */
-void pim_do_grant(struct ga_adj *requests, uint16_t partition_index,
-		struct ga_partd_edgelist *grants);
+void pim_do_grant(struct pim_state *state, uint16_t partition_index);
 
 /**
  * For every source (left-hand) node in partition 'partition_index', select
  *    among its granted edges which edge to accept. These edges are added to
  *    'accepts'
  */
-void pim_do_accept(struct ga_partd_edgelist *grants, uint16_t partition_index,
-		struct ga_adj *tmp_adj, struct ga_partd_edgelist *accepts);
+void pim_do_accept(struct pim_state *state, uint16_t partition_index);
 
-void pim_process_accepts(struct ga_partd_edgelist *accepts,
-		struct ga_adj *requests);
+void pim_process_accepts(struct pim_state *state, uint16_t partition_index);
 
+void pim_add_backlog(struct pim_state *state, );
 
 #endif /* PIM_H_ */

ea57b958f8804c3e05a7897f5d09c69c4f2064fa
Amy Ousterhout <aousterh@mit.edu>
2014-05-18 17:55:28 -0400

    switch arbiter to use 'seq' versions of admissible functions and structures

--- src/arbiter/admission_core.h
+++ src/arbiter/admission_core.h
@@ -1,58 +1,34 @@
+/*
+ * admission_core.h
+ *
+ *  Created on: May 18, 2014
+ *      Author: aousterh
+ */
 
 #ifndef ADMISSION_CORE_H
 #define ADMISSION_CORE_H
 
-#include <stdint.h>
-#include <rte_ring.h>
-#include "main.h"
-#include "control.h"
-#include "../graph-algo/admissible_structures.h"
+#include "seq_admission_core.h"
+#include "../graph-algo/admissible.h"
 
-#define		OVERSUBSCRIBED				0
-#define		INTER_RACK_CAPACITY			4
-#define		OUT_OF_BOUNDARY_CAPACITY	16
-#define		NUM_NODES					256
+static inline
+struct admissible_state *g_admissible_status(void) {
+	return (struct admissible_state *) &g_seq_admissible_status;
+}
 
-#define		ALLOWED_TIMESLOT_LAG		4
+static inline
+struct backlog *g_admission_backlog(void) {
+	return &g_seq_admissible_status.backlog;
+}
 
-#define		ADMITTED_TRAFFIC_MEMPOOL_SIZE	(BATCH_SIZE * 16 * N_ADMISSION_CORES)
-#define		ADMITTED_TRAFFIC_CACHE_SIZE		(2 * BATCH_SIZE)
+static inline
+struct admission_core_statistics *g_admission_core_stats(uint16_t i) {
+	return &g_seq_admissible_status.cores[i].stat;
+}
 
-#define		BIN_MEMPOOL_CACHE_SIZE			(16 * NUM_BINS - 1)
-#define		BIN_MEMPOOL_SIZE				(1024 + 32 * NUM_BINS * N_ADMISSION_CORES)
-
-#define		Q_BIN_RING_SIZE						(32 * NUM_BINS)
-#define		Q_HEAD_RING_SIZE		(64 * 1024)
-#define		URGENT_RING_SIZE		(64 * 1024)
-
-
-/* admissible status */
-extern struct admissible_status g_admissible_status;
-
-/* admitted_traffic pool */
-extern struct rte_mempool* admitted_traffic_pool[NB_SOCKETS];
-
-/* Specifications for controller thread */
-struct admission_core_cmd {
-	uint64_t start_time;
-	uint64_t end_time;
-
-	uint64_t start_timeslot;
-
-	uint32_t admission_core_index; /* the index among admission cores of this one */
-};
-
-void admission_init_global(struct rte_ring *q_admitted_out);
-
-/**
- * Initializes a single core to be a comm core
- */
-void admission_init_core(uint16_t lcore_id);
-
-/**
- * Runs the admission core
- */
-int exec_admission_core(void *void_cmd_p);
-
+static inline
+struct admission_statistics *g_admission_stats(void) {
+	return &g_seq_admissible_status.stat;
+}
 
 #endif /* ADMISSION_CORE_H */

192bcc7123797d4c85e8897b3fc8d685bf6b5e14
Amy Ousterhout <aousterh@mit.edu>
2014-05-18 21:12:47 -0400

    define algo_n_cores for pim

--- src/graph-algo/algo_config.h
+++ src/graph-algo/algo_config.h
@@ -5,15 +5,23 @@
 #ifndef ALGO_CONFIG_H_
 #define ALGO_CONFIG_H_
 
+#define tslot_alloc_add_backlog		lru_alloc_add_backlog
+
+#ifdef PARALLEL_ALGO
+/* parallel algo */
+
+#ifndef ALGO_N_CORES
+#define ALGO_N_CORES				N_PARTITIONS
+#endif
+
+#else
+/* pipelined algo */
+#define PIPELINED_ALGO
+
 #ifndef ALGO_N_CORES
 #define ALGO_N_CORES				4
 #endif
 
-#define tslot_alloc_add_backlog		lru_alloc_add_backlog
-
-#ifndef PARALLEL_ALGO
-/* pipelined algo */
-#define PIPELINED_ALGO
 #endif
 
 #endif /* ALGO_CONFIG_H_ */

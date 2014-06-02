1be5a7e5af394879aed8c101f176e1c714491126
Amy Ousterhout <aousterh@mit.edu>
2014-06-01 23:38:27 -0400

    remove white margins from graph-algo and fairness graphs

--- src/graph-algo/graph_admissible_vs_utilization.R
+++ src/graph-algo/graph_admissible_vs_utilization.R
@@ -101,6 +101,6 @@ ggplot(summary, aes(x=observed_utilization, y=time,
              scale_shape_manual(name="Nodes", guide = guide_legend(reverse = TRUE), values=c(15, 16, 17, 18)) +
              labs(x = "Network utilization (%)",
                   y = expression(paste("Processing time per timeslot (", mu, "s)"))) +
-             coord_cartesian(xlim=c(0,1))
+             coord_cartesian(xlim=c(0,1), ylim=c(0,200))
 
 detach(data)

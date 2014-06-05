1387344e87e859cfde1e0ad3f606a51d3d974faf
Amy Ousterhout <aousterh@mit.edu>
2014-06-04 22:24:09 -0400

    add cpu shielding to stress test script

--- src/arbiter/run.sh
+++ src/arbiter/run.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
-sudo build/fast -c 7e -n 3 --no-hpet -- -p 1
+# check the arguments
+if [ "$#" -ne 1 ]; then
+    # set default value of the mask, for 6 cores
+    MASK=7e
+else
+    MASK="$1"
+fi
+
+sudo build/fast -c $MASK -n 3 --no-hpet -- -p 1

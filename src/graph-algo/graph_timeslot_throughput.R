39f7226e407035a3cc7b524ec441dc7ae971ce90
Amy Ousterhout <aousterh@mit.edu>
2014-06-05 14:00:49 -0400

    added script to parse and plot tslot throughput results

--- src/graph-algo/graph_timeslot_throughput.R
+++ src/graph-algo/graph_timeslot_throughput.R
@@ -9,29 +9,28 @@
 # by manually benchmarking using the stress test core.
 #
 # This script can be run using:
-# R < ./graph_timeslot_throughput.R --save
+# R < ./graph_timeslot_throughput.R <input_file> --save
 
 # use the ggplot2 library
 library(ggplot2)
 
-data <- read.csv("output.csv", sep=",")
+# read in arguments
+args <- commandArgs()
+f = "output.csv"
+if (length(args) > 2) {
+   f <- args[2]
+}
+
+data <- read.csv(f, sep=",")
 
 # call device driver
 pdf(file="timeslot_throughput.pdf", width=6.6, height=3)
 
 theme_set(theme_bw(base_size=12))
 
-BYTES_PER_TIMESLOT = 1500
-BITS_PER_BYTE = 8
-EXPERIMENT_DURATION = 10
-
-data$gbps = data$nodetslots * BYTES_PER_TIMESLOT * BITS_PER_BYTE / (EXPERIMENT_DURATION * 1000 * 1000 * 1000)
-
 data
 
 ggplot(data, aes(x=cores, y=gbps, color=as.factor(batch_size), shape=as.factor(batch_size))) +
-             geom_abline(aes(intercept=0, slope=data$gbps[2]*0.5), linetype="dashed", color="grey") +
-             geom_abline(aes(intercept=0, slope=data$gbps[7]), linetype="dashed", color="grey") +
              geom_point() + geom_line() +
              labs(x = "Cores", y = "Maximum throughput (Gbps)") +
              scale_color_discrete(name="Batch size", guide = guide_legend(reverse = TRUE)) +
---
+++ src/graph-algo/parse_tslot_throughput.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+# this script parses the output of a throughput experiment, generated
+# by running ./measure_tslot_throughput.sh in fastpass/src/controller.
+# it generates a csv output file and a graph of throughput vs. number
+# of cores.
+
+# check the arguments
+if [ "$#" -ne 1 ]; then
+    echo "Usage: $0 <results_directory>"
+    exit 0
+fi
+
+# get the directory name
+DIR="$1"
+
+# create an output file
+# include the timestamp in the dir in the name
+TIMESTAMP=$(echo "$DIR" | cut -d '_' -f3)
+OUTPUT_FILE="tslot_throughput_$TIMESTAMP.csv"
+echo "cores,gbps,algo,batch_size,nodes" > $OUTPUT_FILE
+
+# process each log file in the directory
+echo "reading files from $DIR"
+for f in $( ls $DIR/log_* ); do
+    echo "parsing file $f"
+    
+    # get experiment parameters from the file
+    ALGO_N_CORES=$(grep "admission core" $f | tail -n 1 | cut -d ' ' -f5)
+    BATCH_SIZE=$(grep "admission core" $f | tail -n 1 | cut -d ' ' -f8)
+    NODES=$(grep "admission core" $f | tail -n 1 | cut -d ' ' -f11)
+
+    # get throughput
+    THROUGHPUT=$(grep "best" $f | tail -n 1 | cut -d ' ' -f14)
+    
+    # echo results into file
+    echo "$ALGO_N_CORES,$THROUGHPUT,seq,$BATCH_SIZE,$NODES" >> $OUTPUT_FILE
+done
+
+# plot the results
+R < ./graph_timeslot_throughput.R $OUTPUT_FILE --save

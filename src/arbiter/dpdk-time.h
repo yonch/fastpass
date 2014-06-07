1d9a9638f84be8b7010b47867e93a30b45182540
Jonathan Perry <yonch@yonch.com>
2014-06-07 17:50:24 -0400

    added a DPDK implementation for fp_monotonic_time_ns using rte_get_timer_cycles

--- src/arbiter/dpdk-time.h
+++ src/arbiter/dpdk-time.h
@@ -2,6 +2,8 @@
 #ifndef CONTROLLER_DPDK_TIME_H_
 #define CONTROLLER_DPDK_TIME_H_
 
+#include <rte_cycles.h>
+
 static inline __attribute__((always_inline))
 u64 fp_get_time_ns(void)
 {
@@ -11,6 +13,11 @@ u64 fp_get_time_ns(void)
 		return -1;
 
 	return (1000*1000*1000) * (u64)tp.tv_sec + tp.tv_nsec;
+}
+
+static inline u64 fp_monotonic_time_ns(void)
+{
+	return rte_get_timer_cycles();
 }
 
 
